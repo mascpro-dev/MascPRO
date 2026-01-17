6103c076b24e740a49e3e293985a691db4a09068
mascpro-dev <marceloconelheiros@conexoes.digital>
2026-01-17 10:21:42 -0300

    teste

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-// 1. Importar o Analytics
+// Importação da ferramenta de análise (O erro visual acontece aqui, mas vai funcionar)
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         {children}
-        {/* 2. Ativar o componente aqui no final */}
+        {/* O componente deve ficar aqui, dentro do body */}
         <Analytics />
       </body>
     </html>
