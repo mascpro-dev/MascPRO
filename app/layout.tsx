4b9b2f379f14338dbc363dc13fd139018b44a86b
Marcelo Conelheiros <marceloconelheiros@conexoes.digital>
2026-08-13 16:33:07 -0300

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const inter = Inter({
@@ -34,6 +35,7 @@ export default function RootLayout({
         {children}
         {/* O componente deve ficar aqui, dentro do body */}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
